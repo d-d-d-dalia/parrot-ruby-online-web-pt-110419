a79fb81d775f793651e162db58863c77cc5225f5
dalia sawaya <dalia.sawaya@flatironschool.com>
2020-02-15 04:54:10 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase)
+  if phrase
+    puts "Pretty bird!"
+  end
   puts "Squawk!"
   return "Squawk!"
 end
